c9a8a8ab5103bb183491a2976653b1d87a98a198
EnesOnu <70756495+EnesOnu@users.noreply.github.com>
2021-05-27 14:57:20 +0300

    Yükleme

---
+++ YukselisApp/KULLANICI/Pages/AnaSayfa.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
+
+namespace YukselisApp.KULLANICI.Pages
+{
+    /// <summary>
+    /// AnaSayfa.xaml etkileşim mantığı
+    /// </summary>
+    public partial class AnaSayfa : UserControl
+    {
+        public AnaSayfa()
+        {
+            InitializeComponent();//deneme 25465456
+        }
+
+        private void BTN_KayitTamamla_Click(object sender, RoutedEventArgs e)
+        {
+            IsValidEmail(TXT_KayıtEmail.Text);
+        }
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                
+                return false;
+                
+            }
+        }
+
+        private void BTN_KayitOl_Click(object sender, RoutedEventArgs e)
+        {
+            CRD_Login.Visibility = Visibility.Hidden;
+            CRD_Kayit.Visibility = Visibility.Visible;
+        }
+
+        private void BTN_Geri_Click(object sender, RoutedEventArgs e)
+        {
+            CRD_Login.Visibility = Visibility.Visible;
+            CRD_Kayit.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ YukselisApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YukselisApp.KULLANICI.Pages;
+
+namespace YukselisApp
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CC_Menu.Content = null;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AnaSayfa anaSayfa = new AnaSayfa();
+            CC_Genel.Content = anaSayfa;//
+        }
+
+        private void BTN_Siparis_Click(object sender, RoutedEventArgs e)
+        {
+            CC_Menu.Content = GRD_Siparis;
+            BTN_Siparis.Background = new SolidColorBrush(Color.FromArgb(0xF, 0xFF, 70, 43));
+        }
+    }
+}

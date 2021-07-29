35a808e04e839ae58fa151016dd54c33c74340f7
bilgisayar <bilgisayar@SEMA>
2021-07-29 09:25:21 +0300

    ana sayfa

---
+++ YukselisApp/KULLANICI/Entities/KullaniciEntities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YukselisApp.KULLANICI.Entities
+{
+   public class KullaniciEntities
+    {
+        string AD { get; set; }
+        string SOYAD { get; set; }
+
+        string TC { get; set; }
+
+        string EMAIL { get; set; }
+
+    }
+}
